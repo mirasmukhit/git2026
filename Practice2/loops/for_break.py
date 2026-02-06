11d43320639f2fc76c5e05c2eee5bc713b04a23e
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-06 21:59:13 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/loops/for_break.py
@@ -0,0 +1,24 @@
+for i in range(1, 6):
+    if i == 3:
+        break
+    print(i) #1 2
+
+
+
+
+
+arr = [4, 7, 2, 9, 5]
+for x in arr:
+    if x == 2:
+        print("Found")
+        break
+
+
+
+
+for x in arr:
+    if x == 10:
+        print("Found")
+        break
+else:
+    print("Not found")
