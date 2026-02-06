58a1e9398cb98d24c1adb9972c89061de75fe46b
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-06 22:07:02 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/loops/for_loop.py
@@ -0,0 +1,24 @@
+for i in range(5):
+    print(i) #0 1 2 3 4 (vertical)
+
+
+
+
+
+arr = [10, 20, 30]
+for x in arr:
+    print(x) #10 20 30 (vertical)
+
+
+
+s = "Hello"
+for ch in s:
+    print(ch) #H e l l o  (vertical)
+
+
+for i in range(1, 10, 2): #(start,stop,step)
+    print(i)  #1 3 5 7 9 (vertical)
+
+
+
+
