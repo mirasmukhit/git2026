44290cd9306ca849b56ee07a341c07364a1be814
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-06 22:14:40 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/loops/while_break.py
@@ -0,0 +1,33 @@
+i = 0
+while i < 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1  #0 1 2 3 4
+
+
+
+
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print(n)
+
+
+
+arr = [4, 6, 2, 9]
+i = 0
+while i < len(arr):
+    if arr[i] == 2:
+        print("Found")
+        break
+    i += 1   #Stops when 2 is found.
+
+
+
+
+
+
+
+
