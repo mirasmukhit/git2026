75536603990d0738f9e6123ab77ba026ca9ff417
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-06 20:47:35 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/boolean/boolean_operators.py
@@ -0,0 +1,29 @@
+print(True and False)   # False
+print(False and False)  # False
+
+
+
+
+is_weekend = False
+is_holiday = True
+
+print(is_weekend or is_holiday)  # True
+
+
+
+print(True or False)   # True
+print(False or False)  # False
+
+
+
+
+logged_in = False
+print(not logged_in)  # True
+
+
+
+
+age = 17
+has_permission = True
+
+print(age >= 18 or has_permission)  # True
