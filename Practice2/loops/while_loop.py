e08d41aaf50456331f75bdc99eadb4c097116019
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-06 22:24:42 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice2/loops/while_loop.py
@@ -0,0 +1,26 @@
+i = 0
+while i < 5:
+    print(i) #0 1 2 3 4
+    i += 1
+
+
+
+
+
+while True:
+    print("Hello") #infinite loop  
+    #Runs forever (until you stop it)
+
+
+
+
+
+n = -1
+while n != 0:
+    n = int(input("Enter number: ")) #Keeps asking until you enter 0.
+
+
+
+
+
+
