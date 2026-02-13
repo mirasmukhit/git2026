77537793b28c59f080e50abcc098a3acbbdf4768
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-13 19:37:07 +0500

    Add Practice2 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice3/function/basic_functions.py
@@ -0,0 +1,31 @@
+def my_function():
+    print("Hello from a function")
+my_function()
+my_function()
+my_function()
+
+
+
+def func(a,b):
+    print(a+b)
+func(5,6)
+
+
+def func(number):
+    return(number - 98)/(5-3)
+print(func(100))
+
+
+
+
+
+def get_greeting():
+    return "Hello from a function"
+message = get_greeting()
+print(message)
+
+
+
+
+def my_function(): #Function definitions cannot be empty. If you need to create a function placeholder without any code, use the pass statement:
+    pass
