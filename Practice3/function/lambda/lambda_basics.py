6edc73444430c82edd82dc0eb8c02e5c73986dbc
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-13 20:22:07 +0500

    Add Practice3 - Python control flow examples (boolean, if-else, loops)

---
+++ Practice3/function/lambda/lambda_basics.py
@@ -0,0 +1,17 @@
+x = lambda a : a + 5
+print(x(5))  #10
+
+
+
+
+x = lambda a : a * 8
+print(x(8)) #64
+
+
+
+x = lambda a : a / 4
+print(x(4)) #1.0      if we use   //   output:1 (integer type)
+
+
+x = lambda a : a - 4
+print(x(9))
