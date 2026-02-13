3cbaa8338ae18affdd8511e94dfe7404ef509214
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-14 00:20:24 +0500

    Add Practice3 - Python control flow examples (boolean, if-else, loops)

--- Practice3/function/сlasses.py/init_method.py
+++ Practice3/function/сlasses.py/init_method.py
@@ -48,3 +48,23 @@ p2 = Person("Tobias", 25)
 
 print(p1.name, p1.age) #Emil 18
 print(p2.name, p2.age) #Tobias 25
+
+
+
+
+
+
+
+class Person:
+    def __init__(self, name, age, city, country):
+        self.name = name
+        self.age = age
+        self.city = city
+        self.country = country
+
+p1 = Person("Linus", 30, "Oslo", "Norway")
+
+print(p1.name)
+print(p1.age)
+print(p1.city)
+print(p1.country)
