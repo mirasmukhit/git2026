d6ffb7425f8e921dbcadfd890fe84344c74458b8
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-27 19:34:34 +0500

    Add Practice4)

---
+++ Practice4/math.py
@@ -0,0 +1,48 @@
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+print(x)#5
+print(y)#25
+
+
+
+
+
+
+
+
+
+x = abs(-7.25)
+print(x) #7.25
+
+
+
+
+
+
+x = pow(4, 3)
+print(x) #4^3
+
+
+
+
+import math
+x = math.sqrt(64)
+print(x)#8
+
+
+
+
+import math
+x = math.ceil(1.4) #up
+y = math.floor(1.4) #down
+print(x) # returns 2
+print(y) # returns 1
+
+
+
+
+
+
+import math
+x = math.pi
+print(x)#3.14.....
