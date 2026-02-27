cd19ef07e1299b5e7e940e6d1a5f2fa7cf9541e2
mirasmukhit <mirasmukhit0874@gmail.com>
2026-02-27 19:30:50 +0500

    Add Practice4)

---
+++ Practice4/dates.py
@@ -0,0 +1,22 @@
+import datetime
+x = datetime.datetime.now()
+print(x)#2026-02-27 19:22:02.800495
+
+
+
+
+import datetime
+x = datetime.datetime.now()
+print(x.year)#2026
+print(x.strftime("%A"))#Friday
+
+
+
+import datetime
+x = datetime.datetime(2020, 5, 17)
+print(x)
+
+
+
+
+
--- Practice4/tempCodeRunnerFile.py
+++ Practice4/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-5
+strftime("%Y-%m-%d %H:%M:%S")
