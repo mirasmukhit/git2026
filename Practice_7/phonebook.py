49383d1e3626f65f289e8a0c5396219150cd54e1
mirasmukhit <mirasmukhit0874@gmail.com>
2026-03-28 12:40:01 +0500

    Add Practice7

--- Practice_7/phonebook.py
+++ Practice_7/phonebook.py
@@ -1,4 +1,5 @@
 import psycopg2
+import csv
 conn = psycopg2.connect(
     host = "localhost",
     dbname = "pp2_db",
@@ -13,11 +14,16 @@ cur.execute("""
         email       VARCHAR(100)    NOT NULL,
         phone_number    VARCHAR(100)    UNIQUE NOT NULL);
             """)
-cur.execute("""
-insert into phonebook(name,email,phone_number)
-            values('Aaa','a@gmail.com','8777777'),
-            ('Bbb','b@gmail.com',8887777);
-""")
+with open("Practice_7/contacts.csv", "r") as f:
+    reader = csv.reader(f)
+    next(reader)  # skip header
+
+    for row in reader:
+        cur.execute("""
+            INSERT INTO phonebook (name, email, phone_number)
+            VALUES (%s, %s, %s)
+            ON CONFLICT DO NOTHING;
+        """, row)
 conn.commit()
 print("Table created")
 cur.close()
